15822c08ee58c86badc0a9e1e31fdce83cff1784
chlee <birdcross10@gmail.com>
2026-07-31 09:17:45 +0900

    장바구니 페이지 디자인 및 CSS 충돌 수정

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub 저장소 이름이 ssagda일 때 필요
   base: '/ssagda/',
+
+  server: {
+    port: 5173,
+    open: '/ssagda/',
+  },
 });
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     open: true,
-//   },
-// });
